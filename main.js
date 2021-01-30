d4b674f0cf88ffb255a7ef038f5c6dafca19ff9b
khushi-kothari <62203565+khushi-kothari@users.noreply.github.com>
2021-01-30 08:32:35 +0530

    Color Picker project!
    Did you know that doing this simple thing in html would create a color picker?
    <input type = "color">
    You don't need js for this!!

---
+++ main.js
@@ -0,0 +1,5 @@
+function myfunction() {
+    var color = document.getElementById("favcolor").value;
+    document.getElementById("fluid-container").style.backgroundColor = color;
+    document.getElementById("button").style.backgroundColor = color;
+}
