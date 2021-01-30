96308608d8f0af274c533f4ff4fc77b65224018d
khushi-kothari <62203565+khushi-kothari@users.noreply.github.com>
2021-01-30 10:30:40 +0530

    Updated main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+alert('Choose a color then click anywhere in the background');
 function myfunction() {
     var color = document.getElementById("favcolor").value;
     document.getElementById("fluid-container").style.backgroundColor = color;
